239b8568d825a7575a72fb576b594af3bcb109aa
weasl <iacobserban17danieloff@gmail.com>
2025-05-16 15:31:18 +0300

    fixed navbar bugs

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import NavbarLogo from "./NavbarLogo";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-gray-800 p-4 shadow-md z-50">
-      <div className="container mx-auto flex justify-between items-center">
+    <nav className="fixed top-0 left-0 right-0 bg-gray-800 p-4 shadow-md z-50 w-full">
+      <div className="container mx-auto flex justify-between items-center px-4">
         <NavbarLogo />
         <NavbarUserSection />
       </div>
